e5e97fec1a229180e8d6acb5fd1f62cb5582c787
Buldacius <slipknot25252566@mail.ru>
2021-06-17 16:07:24 +0300

    Пробую 2

--- u/src/dfasd.java
+++ u/src/dfasd.java
@@ -1,5 +1,8 @@
 public class dfasd {
     public static void main(String[] args) {
         System.out.println("dsfsdfsdfs");
+        System.out.println("dsfsdfsdfs");
+        System.out.println("dsfsdfsdfs");
+        System.out.println("dsfsdfsdfs");
     }
 }
